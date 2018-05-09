7e60a745694cfa2fe09e1171bef91725217309f9
Canto <acanto95@bitbucket.org>
2018-05-09 17:16:33 -0500

    modificacion archivo

--- angularTesting/src/app/viajes-cards/viajes-cards.component.ts
+++ angularTesting/src/app/viajes-cards/viajes-cards.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import {Http, Response} from '@angular/http';
 import 'rxjs/add/operator/map';
+import 'rxjs/Rx';
+
 
 @Component({
   selector: 'app-viajes-cards',
